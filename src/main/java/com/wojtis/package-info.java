a5fa29d2a2a8b1bfbf9ae8168803aeb5c270b8b4
wojtis <wojtis85@gmail.com>
2019-05-31 22:21:22 +0200

    add maven plugin for xsd generation

--- src/main/java/com/wojtis/package-info.java
+++ src/main/java/com/wojtis/package-info.java
@@ -1,9 +1,13 @@
 @XmlAccessorType(XmlAccessType.PROPERTY)
 @XmlSchemaType(type = Date.class, name ="date")
+//namespace needed for jaxb2-maven-plugin to rename output file
+//@XmlSchema(namespace = "http://www.wojtis.com/jaxb", elementFormDefault = XmlNsForm.QUALIFIED)
 package com.wojtis;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
 import javax.xml.bind.annotation.XmlSchemaType;
 import java.util.Date;
 
